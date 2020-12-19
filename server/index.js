5900b308ba57486c3470603d6f62593c1e0dcd5c
Rahul Yadav <ryrahul_yadav@outlook.com>
2020-12-20 02:02:05 +0530

    package change

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 const streamURL =
   'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
 
-const rules = [{ value: 'coding' }];
+const rules = [{ value: 'flutter' }];
 
 // Get stream rules
 async function getRules() {
