367e9d284ec354be6da34431b1630a043b694154
Rahul Yadav <ryrahul_yadav@outlook.com>
2021-09-24 13:39:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 const streamURL =
   'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
 
-const rules = [{ value: 'Oxygen Noida Verified' }];
+const rules = [{ value: 'SAFEMOON' }];
 
 // Get stream rules
 async function getRules() {
